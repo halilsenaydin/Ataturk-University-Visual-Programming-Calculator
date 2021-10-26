7ecb5b95e77d4a5b885b2207cc8e9c8f08563aab
snydnhll <snydnhll@hotmail.com>
2021-10-26 08:16:55 +0300

    Fixed OperationManager

--- Operations/Concretes/OperationManager.cs
+++ Operations/Concretes/OperationManager.cs
@@ -12,6 +12,11 @@ namespace Operations.Concretes
         public double _number { get; set; }
         private double _result { get; set; }
 
+        private double previousNumber { get; set; }
+        private double newNumber { get; set; }
+
+        private int numberOfTransactionsMade { get; set; }
+
         public OperationManager()
         {
             _result = 0;
@@ -30,34 +35,38 @@ namespace Operations.Concretes
         public void ClearResult()
         {
             _result = 0;
+            numberOfTransactionsMade = 0;
         }
 
         public IDataResult<double> Adding(double number)
         {
-            _number = number;
-            _result += _number;
+            numberOfTransactionsMade++;
+            previousNumber = newNumber;
+            newNumber = number;
+
+            _result += newNumber;
             return new SuccessDataResult<double>(_result);
         }
 
         public IDataResult<double> Division(double number) 
         {
+            numberOfTransactionsMade++;
             _number = number;
             _result /= _number;
             return new SuccessDataResult<double>(_result);
         }
 
-        public IDataResult<double> Multiplication(double number) // Toplama işlemi  ile çarpma..
+        public IDataResult<double> Multiplication(double number)
         {
+            if (numberOfTransactionsMade == 0)
+            {
+                _result = 1;
+            }
+
+            numberOfTransactionsMade++;
             _number = number;
 
-            double smallest = Math.Min(_result, number);
-            double biggest = Math.Max(_result, number);
-
-            for (int i = 0; i < smallest; i++)
-            {
-                _result += biggest;
-            }
-            
+            _result *= _number;
             return new SuccessDataResult<double>(_result);
         }
 
@@ -65,12 +74,9 @@ namespace Operations.Concretes
         {
             _number = number;
             var result = Adding((_number)*-1);
-            if (result.Success)
-            {
-                return new SuccessDataResult<double>(_result);
-            }
+            _result = result.Data;
 
-            return new ErrorDataResult<double>(_result);
+            return new SuccessDataResult<double>(_result);
         }
     }
 }
